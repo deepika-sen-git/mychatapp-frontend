8501ed5fdd1227dcd58945eb2c20ba24a3a8f6f7
deepika-sen-git <147013559+deepika-sen-git@users.noreply.github.com>
2025-09-15 11:19:19 +0530

    Add files via upload

---
+++ src/assets/docs/privacyPolicyContent.ts
@@ -0,0 +1,69 @@
+// privacyPolicyContent.ts
+const privacyPolicyContent = [
+    {
+        date: '08/JAN/2025',
+        paragraph: `At MyChat, your privacy is a priority. This Privacy Policy explains how we collect, use, and protect your information when you use the MyChat application ("App"). By using MyChat, you agree to the practices described in this Privacy Policy. If you do not agree, please do not use the App.`
+    },
+    {
+        heading: '1. Information We Collect',
+        list: [
+            {
+                title: 'Personal Information:',
+                subList: [
+                    'Email address, phone number, and unique user ID during account registration.',
+                    'Profile details, such as your profile name and profile picture.'
+                ]
+            },
+            {
+                title: 'Usage Data:',
+                subList: [
+                    'Information about your interactions with the App, such as chat history and group activity.'
+                ]
+            },
+            {
+                title: 'Device Information:',
+                subList: [
+                    'Device type, operating system, IP address, and app version.'
+                ]
+            },
+            {
+                title: 'Media and Files:',
+                subList: [
+                    'Photos, videos, and other content you share through the App.'
+                ]
+            }
+        ]
+    },
+    {
+        heading: '2. How We Use Your Information',
+        paragraph: 'We use your information to:',
+        list: [
+            'Provide and improve the MyChat App and its features.',
+            'Facilitate communication between you and your contacts.',
+            'Ensure account security through two-step verification and password recovery.',
+            'Display personalized advertising banners within the App.',
+            'Monitor and analyze app usage to enhance user experience.'
+        ]
+    },
+    {
+        heading: '3. Sharing Your Information',
+        paragraph: 'We do not sell your personal information to third parties. However, we may share your information with:',
+        list: [
+            'Service Providers: Third - party vendors who help us operate and maintain the App.',
+            'Legal Authorities: When required by law or to protect the rights, safety, and security of users.',
+        ]
+    },
+    {
+        heading: '4. Contact Us',
+        paragraph: 'If you have questions or concerns about this Privacy Policy, please contact us at privacy@mychat.com.',
+        list: [
+            'Service Providers: Third - party vendors who help us operate and maintain the App.',
+            'Legal Authorities: When required by law or to protect the rights, safety, and security of users.',
+        ]
+    },
+    {
+        paragraph: 'Your use of MyChat is governed by our Privacy Policy. By using the App, you agree to the collection, storage, and use of your personal data as outlined in the Privacy Policy.'
+    }
+];
+
+export default privacyPolicyContent;
